1243e33a88469115350c9e520fa407f607ffbef5
martinKindall <mart256@gmail.com>
2022-05-21 19:21:50 +0200

    Added Extract Lambda

--- lib/etl-patterns-stack.ts
+++ lib/etl-patterns-stack.ts
@@ -1,5 +1,6 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
@@ -7,6 +8,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as logs from 'aws-cdk-lib/aws-logs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class EtlPatternsStack extends Stack {
@@ -15,8 +17,10 @@ export class EtlPatternsStack extends Stack {
   private queue: sqs.Queue;
   private table: dynamodb.Table;
   private bucket: s3.Bucket;
+  private vpc: ec2.Vpc;
   private cluster: ecs.Cluster;
   private taskDefinition: ecs.TaskDefinition;
+  private container: ecs.ContainerDefinition;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -30,6 +34,7 @@ export class EtlPatternsStack extends Stack {
     });
 
     this.fargateSetup();
+    this.lambdasSetup();
   }
 
   private createResources() {
@@ -46,7 +51,7 @@ export class EtlPatternsStack extends Stack {
   }
 
   private fargateSetup() {
-    const vpc = new ec2.Vpc(this, 'Vpc', {
+    this.vpc = new ec2.Vpc(this, 'Vpc', {
       maxAzs: 2
     });
 
@@ -55,7 +60,7 @@ export class EtlPatternsStack extends Stack {
       logRetention: logs.RetentionDays.ONE_WEEK
     });
 
-    this.cluster = new ecs.Cluster(this, 'EcsCluster', {vpc});
+    this.cluster = new ecs.Cluster(this, 'EcsCluster', {vpc: this.vpc});
 
     this.taskDefinition = new ecs.FargateTaskDefinition(this, 'FargateTaskDefinition', {
       memoryLimitMiB: 512,
@@ -66,7 +71,7 @@ export class EtlPatternsStack extends Stack {
 
     this.bucket.grantRead(this.taskDefinition.taskRole);
 
-    const container = this.taskDefinition.addContainer('AppContainer', {
+    this.container = this.taskDefinition.addContainer('AppContainer', {
       image: ecs.ContainerImage.fromAsset('container/dataExtractionTask'),
       logging,
       environment: {
@@ -74,5 +79,49 @@ export class EtlPatternsStack extends Stack {
         'S3_OBJECT_KEY': ''
       }
     });
+  }
+
+  private lambdasSetup() {
+    const extractLambda = new lambda.Function(this, 'extractLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-fns/extract'),
+      handler: 's3SqsEventConsumer.handler',
+      reservedConcurrentExecutions: this.LAMBDA_THROTTLE_SIZE,
+      environment: {
+        CLUSTER_NAME: this.cluster.clusterName,
+        TASK_DEFINITION: this.taskDefinition.taskDefinitionArn,
+        SUBNETS: JSON.stringify(Array.from(this.vpc.privateSubnets, x => x.subnetId)),
+        CONTAINER_NAME: this.container.containerName
+      }
+    });
+
+    this.queue.grantConsumeMessages(extractLambda);
+    extractLambda.addEventSource(new SqsEventSource(this.queue, {}));
+    extractLambda.addToRolePolicy(this.eventBridgePutPolicy);
+
+    const runTaskPolicyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [
+        this.taskDefinition.taskDefinitionArn
+      ],
+      actions: [
+        'ecs:RunTask'
+      ]
+    });
+
+    extractLambda.addToRolePolicy(runTaskPolicyStatement);
+
+    const taskExecutionRolePolicyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'iam:PassRole'
+      ],
+      resources: [
+        this.taskDefinition.obtainExecutionRole().roleArn,
+        this.taskDefinition.taskRole.roleArn
+      ]
+    });
+
+    extractLambda.addToRolePolicy(taskExecutionRolePolicyStatement);
   }
 }
