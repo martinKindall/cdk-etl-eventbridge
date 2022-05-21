b7074d6303656bf91220f40f5334fff65a62611f
martinKindall <mart256@gmail.com>
2022-05-21 18:14:55 +0200

    Added bucket, table

--- lib/etl-patterns-stack.ts
+++ lib/etl-patterns-stack.ts
@@ -1,13 +1,23 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class EtlPatternsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EtlPatternsQueue', {
+    const queue = new sqs.Queue(this, 'newObjectInBucketEventQueue', {
       visibilityTimeout: Duration.seconds(300)
     });
+
+    const LAMBDA_THROTTLE_SIZE = 2;
+
+    const table = new dynamodb.Table(this, 'TransformedData', {
+      partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING}
+    });
+
+    const bucket = new s3.Bucket(this, 'LandingBucket', {});
   }
 }
